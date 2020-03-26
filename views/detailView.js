8e561adaa58e1556a28db987153a3e0397ef6ca1
Murat0901 <menzilcim@gmail.com>
2020-03-26 12:37:10 +0300

    react native vector icons eklendi

--- views/detailView.js
+++ views/detailView.js
@@ -1,12 +1,12 @@
 import {View, Text} from 'react-native';
 import * as React from 'react';
 
-function historyView() {
+function detailView() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Arama geçmişi sayfasındasınız</Text>
+      <Text>Detay sayfasına geldiniz.</Text>
     </View>
   );
 }
 
-export default historyView;
+export default detailView;
--- views/homePageView.js
+++ views/homePageView.js
@@ -1,11 +1,14 @@
 import {View, Text} from 'react-native';
 import * as React from 'react';
+import Icon from 'react-native-vector-icons/AntDesign'
 
 function HomeScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Anasayfama Hoş Geldin!</Text>
       <Text>Murat Menzilci'nin tasarımıdır.</Text>
+      <Text style={{alignItems:'center', color:'red'}}> Merhabalar.</Text>
+      <Icon name="search1" size={30} color="blue" />
     </View>
   );
 }
